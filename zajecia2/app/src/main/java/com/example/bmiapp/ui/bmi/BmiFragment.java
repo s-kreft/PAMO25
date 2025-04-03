e2ebf022b2757f09faefc70069b9197203999aa0
s-kreft <sebastian.kreft.sg@gmail.com>
2025-04-03 20:14:07 +0200

    Add bmi calculation to the project

--- zajecia2/app/src/main/java/com/example/bmiapp/ui/bmi/BmiFragment.java
+++ zajecia2/app/src/main/java/com/example/bmiapp/ui/bmi/BmiFragment.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import com.example.bmiapp.R;
+import androidx.annotation.Nullable;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -12,9 +16,22 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.bmiapp.databinding.FragmentBmiBinding;
 
+import java.text.DecimalFormat;
+
 public class BmiFragment extends Fragment {
 
     private FragmentBmiBinding binding;
+    private double heightValue = 0.0;
+    private double weightValue = 0.0;
+    private double bmiValue = 0.0;
+    private String bmiStatus = "";
+
+    private EditText haightInput;
+    private EditText weightInput;
+    private Button calculateButton;
+    private TextView bmiTextValue;
+    private TextView bmiTextStatus;
+    DecimalFormat df = new DecimalFormat("#.##");
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,14 +41,40 @@ public class BmiFragment extends Fragment {
         binding = FragmentBmiBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        haightInput = (EditText) getView().findViewById(R.id.haightInput);
+        weightInput = (EditText) getView().findViewById(R.id.weightInput);
+        calculateButton = (Button) getView().findViewById(R.id.calculateButton);
+        bmiTextValue = (TextView) getView().findViewById(R.id.bmiValue);
+        bmiTextStatus = (TextView) getView().findViewById(R.id.bmiStatus);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                heightValue = Double.parseDouble(haightInput.getText().toString());
+                weightValue = Double.parseDouble(weightInput.getText().toString());
+                calculateBMI();
+                bmiTextValue.setText("BMI: " + df.format(bmiValue));
+                bmiTextStatus.setText(bmiStatus);
+            }
+        });
+    }
+    private void calculateBMI() {
+        bmiValue = (weightValue / Math.pow(heightValue, 2));
+        if(bmiValue < 18.5) {
+            bmiStatus = "NIEDOWAGA";
+        } else if (bmiValue >= 18.5 & bmiValue < 24.99) {
+            bmiStatus = "optimum";
+        } else if (bmiValue >= 25 & bmiValue < 29.99) {
+            bmiStatus = "NADWAGA";
+        } else if (bmiValue >= 30) {
+            bmiStatus = "OTYŁOŚĆ";
+        }
     }
 }
